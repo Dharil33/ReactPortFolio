98e3cab92770110e0de8e9a18fe6fdc321bc33e7
Dharil33 <59661721+Dharil33@users.noreply.github.com>
2021-08-02 19:13:01 +0530

    Update Commom.jsx

--- src/Commom.jsx
+++ src/Commom.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+import Typical from "react-typical";
+import { motion } from 'framer-motion';
 const Commom = (props) => {
   return (
     <>
@@ -10,15 +11,28 @@ const Commom = (props) => {
             <div className="col-10 mx-auto">
               <div className="row">
                 <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
-              <h1>
-                    {props.name}<br/>
-                    <strong className="brand-name"> I'm Dharil Patel </strong>
-                  </h1>
-                  <div className="mt-3">
+                  <motion.h1 animate = {{fontSize:30}}> {props.name}<br/>
+                    <strong className="brand-name">
+                       I'm 
+
+                    <Typical loop={Infinity} wrapper="b"
+
+                  steps={[
+                    ' Dharil Patel',
+                    1000,
+                    ' Full Stack Developer',
+                    1000,
+                    ' Computer Engineer',
+                    1000,
+                  ]}
+                  />
+                   </strong>
+                   </motion.h1>
+                  <motion.div className="mt-3" initial={{x:'-100vw'}} animate={{x:1}} transition={{delay:1,duration:1.5}} whileHover={{scale:1.1,boxShadow:"0px rgb(255,255,255)"}}>
                     <NavLink to={props.visit} className="btn-get-started ">
                       {props.btname}
                     </NavLink>
-                  </div>
+                  </motion.div>
                 </div>
 
                 <div className="col-lg-6 order-1 order-lg-2 header-img">
