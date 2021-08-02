1322bb3515266c8a29948c5b65e481841c14d28a
Dharil33 <59661721+Dharil33@users.noreply.github.com>
2021-08-02 19:14:33 +0530

    Update Service.jsx

--- src/Service.jsx
+++ src/Service.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Sdata from "./Sdata";
 import Card from "./Card";
+import {motion} from "framer-motion";
 
 const Services = () => {
   return (
     <>
       <div className="my-5">
-        <h1 className="text-center"> My Projects </h1>
+        <motion.h1 className="text-center" initial={{y:-250}} animate={{y:-10}} transition={{delay:0.5,type:"spring",stiffness:120}}> My Projects </motion.h1>
       </div>
       <div className="container-fluid mb-5">
         <div className="row">
           <div className="col-10 mx-auto">
-            <div className="row gy-4">
+            <div className="row gy-4" id="project">
               {Sdata.map((val, ind) => {
                 return <Card key={ind} imgsrc={val.imgsrc} title={val.title} text={val.text} />;
               })}
