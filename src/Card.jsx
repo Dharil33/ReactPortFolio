245003964c915dafd63c3d0eb668a3ed3fab879d
Dharil33 <59661721+Dharil33@users.noreply.github.com>
2021-08-02 19:12:18 +0530

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import {motion} from 'framer-motion';
+import Reactpage from './Reactpage';
 
 const Card = (props) => {
   return (
     <>
       <div className="col-md-4 col-12 mx-auto">
-        <div className="card">
+        <motion.div className="card" initial={{x:'100vw'}} animate={{x:0}} transition={{delay:0.8}} whileHover={{scale:1.1,boxShadow:"0px 0px 10px rgb(255,255,255)"}}>
           <img src={props.imgsrc} className="card-img-top" alt={props.imgsrc} />
           <div className="card-body">
             <h5 className="card-title font-weight-bold">{props.title}</h5>
@@ -13,7 +14,7 @@ const Card = (props) => {
               {props.text}
             </p>
           </div>
-        </div>
+        </motion.div>
       </div>
     </>
   );
