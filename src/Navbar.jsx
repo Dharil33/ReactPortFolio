17ac6864b356779b35d39b5e50bc61c937caba88
Dharil33 <59661721+Dharil33@users.noreply.github.com>
2021-08-02 19:13:59 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { motion } from 'framer-motion';
+import logo from './images/logo.jpg';
 
 const Navbar = () => {
   return (
     <>
-      <div className="container-fluid nav_bg">
+      <motion.div className="container-fluid nav_bg" initial={{y:-250}} animate={{y:-10}} transition={{delay:0.6,type:"spring",stiffness:400}}>
         <div className="row">
           <div className="col-12 mx-auto">
-            <nav className="navbar navbar-expand-lg navbar-light">
+            <nav className="navbar navbar-expand-lg navbar">
               <div className="container-fluid">
                 <NavLink className="navbar-brand" to="/">
-                  Portfolio
+                  <motion.img src={logo} className="mylogo" alt="mylogo" style={{boxShadow:"0px 0px 10px rgba(255,255,255)"}}/>
                 </NavLink>
                 <button
                   className="navbar-toggler"
@@ -74,7 +76,7 @@ const Navbar = () => {
             </nav>
           </div>
         </div>
-      </div>
+      </motion.div>
     </>
   );
 };
